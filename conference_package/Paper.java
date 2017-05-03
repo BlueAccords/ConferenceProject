1822a24bee67187b3321fc280c9577f4abb1eaa5
AyubT <atiba@uw.edu>
2017-05-03 12:04:08 -0700

    eight commit, branch now equivelent to master

---
+++ conference_package/Paper.java
@@ -0,0 +1,118 @@
+package conference_package;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.io.Serializable;
+
+/**
+ * This class represents a manuscript and stores any relevant information.
+ * @author James Roberts, Ian Waak, Ayub Tiba
+ * @version 4/30/2017
+ */
+public class Paper implements Serializable{
+
+	/**
+	 * The serial Id number
+	 */
+	private static final long serialVersionUID = 96995919879996851L;
+	/**
+	 * The paper's title.
+	 */
+	private String myTitle;
+	/**
+	 * The body of the paper.
+	 */
+	private String myPaper;
+	/**
+	 * The list of authors by username, index 0 is the primary author.
+	 */
+	private ArrayList<String> myAuthors;
+	/**
+	 * When the paper was submitted.
+	 */
+	private Date mySubmissionDate;
+	/**
+	 * Constructor for The class.
+	 * @param theTitle The paper's title.
+	 * @param thePaper The paper's body.
+	 * @param theMainAuthor The main author of the paper's username.
+	 * @author Ayub Tiba
+	 * @version 4/27/2017
+	 */
+	public Paper(String theTitle, String thePaper, String theMainAuthor) {
+		myTitle = theTitle;
+		myAuthors = new ArrayList<String>();
+		myAuthors.add(theMainAuthor);
+		myPaper = thePaper;
+		mySubmissionDate = new Date();
+		
+	}
+	
+	/**
+	 * Returns the date the paper was submitted.
+	 * @return the date the paper was submitted.
+	 */
+	public Date getSubmissionDate() {
+		return new Date(mySubmissionDate.getTime());
+	}
+	/**
+	 * Setter for the date the paper was submitted.
+	 * @author Ian Waak
+	 * @version 4/30/2017
+	 */
+	public void setSubmissionDate(Date theDate) {
+		mySubmissionDate = theDate;
+	}
+	/**
+	 * Getter for the paper's title.
+	 * @return The title of the paper.
+	 * @author James Roberts
+	 * @version 4/27/2017
+	 */
+	public String getTitle() {
+		return myTitle;
+	}
+	
+	/**
+	 * Getter for the paper.
+	 * @return The body of the paper.
+	 * @author James Roberts
+	 * @version 4/27/2017
+	 */
+	public String getPaper() {
+		return myPaper;
+	}
+	
+	/**
+	 * Getter for the list of author's
+	 * @return The paper's authors.
+	 * @author James Roberts
+	 * @version 4/27/2017
+	 */
+	public ArrayList<String> getAuthors() {
+		ArrayList<String> copy = new ArrayList<String>();
+		copy.addAll(myAuthors);
+		return copy;
+	}
+	
+	/**
+	 * Adds a co-author to the paper.
+	 * @param theAuthor The co-author to be added.
+	 * @author James Roberts
+	 * @version 4/27/2017
+	 */
+	public void addAuthor(String theAuthor) {
+		myAuthors.add(theAuthor);
+	}
+	
+	/**
+	 * Replaces the body of the paper with an updated version.
+	 * @param thePaper The new paper body.
+	 * @author James Roberts
+	 * @version 4/27/2017
+	 */
+	public void updatePaper(String thePaper) {
+		myPaper = thePaper;
+	}
+	
+}
---
+++ conference_package/SubprogramChair.java
@@ -0,0 +1,21 @@
+package conference_package;
+
+import java.util.ArrayList;
+
+public class SubprogramChair extends View {
+	private ArrayList<Paper> papers;
+	
+	public void SubporgramChair (ArrayList<Paper> thePapers) {
+		papers = thePapers;
+	}
+	
+	public void displayMenu() {
+		System.out.println("/-------\\	       __  __");
+		System.out.println("|--MSE--|	|\\ / ||__ |__");
+		System.out.println("\\-------/	| \\/ | __||__");
+		System.out.println("-------------------------");
+		for (int i = 0; i < papers.size(); i++) { 
+			
+		}
+	}
+}
