05f8ee4af3179b96d8cf627b94f702209f43e342
cga3798 <fightnyank@live.com>
2017-05-16 22:11:40 -0700

    started Author UI class

---
+++ client/UI_Author.java
@@ -0,0 +1,18 @@
+package client;
+
+import javax.swing.JPanel;
+
+public class UI_Author extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -703649288417940022L;
+	
+	public UI_Author() {
+	
+	}
+	
+	
+	
+}
