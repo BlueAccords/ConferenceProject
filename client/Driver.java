9ad12bd2c9f5f7b00b3c5f5788e5c754a4266999
milk9111 <connorlndbrg349@gmail.com>
2017-05-23 22:08:47 -0700

    Made test data, details are commented in Driver

--- client/Driver.java
+++ client/Driver.java
@@ -1,11 +1,36 @@
 package client;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import javax.swing.JPanel;
+
+import model.Conference;
+import model.User;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		Controller systemController = new Controller();
+		/*User testUser1 = new User("john@email.com");
+		User testUser2 = new User("connor@test.com");
+		User testUser3 = new User("casanova@test.com");
+		User testUser4 = new User("mdanger@test.com");
+		User testUser5 = new User("jmoney@test.com");
+		User testUser6 = new User("lilryerye@test.com");
+		ArrayList<User> testUsers = new ArrayList<User>();
+		testUsers.add(testUser6);
+		testUsers.add(testUser5);
+		testUsers.add(testUser4);
+		testUsers.add(testUser3);
+		testUsers.add(testUser2);
+		testUsers.add(testUser1);
+		User.writeUsers(testUsers);
+		
+		ArrayList<Conference> exampleList = new ArrayList<Conference>();
+		exampleList.add(new Conference("ACM Conference", new Date(), new Date(), new Date(), new Date()));
+		Conference.writeConferences(exampleList);*/
+	
 		
 		systemController.startProgram();
 	}
