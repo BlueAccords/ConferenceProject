19e7cf1b6d8b737c2adc8a1ca7ec65388cd686d3
BlueAccords <myoldemailname@gmail.com>
2017-05-27 16:06:26 -0700

    added buttons to manuscript list view

--- client/ManuscriptListTableView.java
+++ client/ManuscriptListTableView.java
@@ -1,5 +1,8 @@
 package client;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+
 /*
  * TableDemo.java requires no other files.
  */
@@ -8,6 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
@@ -19,6 +23,8 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Observable;
 
@@ -31,15 +37,42 @@ import java.util.Observable;
  * @author Ryan Tran
  *
  */
-public class ManuscriptListTableView extends Observable {
+public class ManuscriptListTableView extends Observable implements ActionListener {
     private boolean DEBUG = false;
     private JPanel myPanel;
+    private JPanel myButtonPanel;
+    private JScrollPane myManuscriptListScrollPane;
+    
+    private JButton myAddNewManuscriptBtn;
+    private JButton myDeleteManuscriptBtn;
+    private JButton myViewMoreInfoBtn;
+    private JButton myDownloadBtn;
+    
+    /**
+     *  used to disable action buttons until a table row is selected
+     */
+    private boolean myManuscriptRowIsSelected = false;
+    
+    /**
+     * Used to store the currently selected manuscript from the manuscript table.
+     * This is the manuscript that will be passed to the controller on add or delete
+     */
+    private Manuscript myCurrentlySelectedManuscript;
+    
+    public static final String ADD_NEW_MANUSCRIPT = "ADD_NEW_MANUSCRIPT";
+    public static final String DELETE_MANSUCRIPT = "DELETE_MANUSCRIPT";
+    public static final String VIEW_MORE_INFO = "VIEW_MORE_INFO";
+    public static final String DOWNLOAD_MANUSCRIPT = "DOWNLOAD_MANUSCRIPT";
+
     private ArrayList<Manuscript> myCurrentManuscriptList;
  
     public ManuscriptListTableView(ArrayList<Manuscript> theManuscriptList) {
     	myCurrentManuscriptList = theManuscriptList;
     	myPanel = new JPanel(new BorderLayout());
  
+        /**
+         * Set up and add Main Manuscript List Table
+         */
         JTable table = new JTable(new MyTableModel());
         table.setPreferredScrollableViewportSize(new Dimension(500, 200));
         table.setFillsViewportHeight(true);
@@ -62,9 +95,19 @@ public class ManuscriptListTableView extends Observable {
 
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
-				// Only manuscript row selection should set currentManuscript
+				// On table row select set currently selected manuscript field to
+				// selected manuscript
 				if (!e.getValueIsAdjusting()) {
-					System.out.println(myCurrentManuscriptList.get(table.getSelectedRow()).getTitle() + " is Selected");
+					// Enable action buttons to be clickable if they are disabled.
+					if(!myAddNewManuscriptBtn.isEnabled()) {
+						myAddNewManuscriptBtn.setEnabled(true);
+						myDeleteManuscriptBtn.setEnabled(true);
+						myViewMoreInfoBtn.setEnabled(true);
+						myDownloadBtn.setEnabled(true);
+					}
+					
+					Manuscript selectedManu = myCurrentManuscriptList.get(table.getSelectedRow());
+					setCurrentlySelectedManuscript(selectedManu);
 			    }
 				
 			}
@@ -72,16 +115,105 @@ public class ManuscriptListTableView extends Observable {
 		});
  
         //Create the scroll pane and add the table to it.
-        JScrollPane scrollPane = new JScrollPane(table);
+        myManuscriptListScrollPane = new JScrollPane(table);
  
         //Add the scroll pane to this panel.
-        myPanel.add(scrollPane, BorderLayout.CENTER);
+        myPanel.add(myManuscriptListScrollPane, BorderLayout.CENTER);
+        
+        
+        /**
+         * Setup South Button Panel
+         */
+        myButtonPanel = new JPanel();
+        myButtonPanel.setLayout(new BoxLayout(myButtonPanel, BoxLayout.LINE_AXIS));
+        myButtonPanel.setBorder(new EmptyBorder(50, 25, 50, 25));
+
+        // add buttons to btn panel
+        // by default buttons are disabled until a row is selected
+        EmptyBorder btnBorders = new EmptyBorder(10, 5, 10, 5);
+        
+        this.myAddNewManuscriptBtn = new JButton("Add New Manuscript...");
+        this.myAddNewManuscriptBtn.setEnabled(false);
+        this.myAddNewManuscriptBtn.addActionListener(this);
+        this.myAddNewManuscriptBtn.setActionCommand(this.ADD_NEW_MANUSCRIPT);
+
+        this.myDeleteManuscriptBtn = new JButton("Delete Manuscript");
+        this.myDeleteManuscriptBtn.setEnabled(false);
+		this.myDeleteManuscriptBtn.addActionListener(this);
+		this.myDeleteManuscriptBtn.setActionCommand(this.DELETE_MANSUCRIPT);
+
+
+        this.myViewMoreInfoBtn = new JButton("View More Info");
+        this.myViewMoreInfoBtn.setEnabled(false);
+        this.myViewMoreInfoBtn.addActionListener(this);
+        this.myViewMoreInfoBtn.setActionCommand(this.VIEW_MORE_INFO);
+
+		this.myDownloadBtn = new JButton("Download");
+        this.myDownloadBtn.setEnabled(false);
+        this.myDownloadBtn.addActionListener(this);
+        this.myDownloadBtn.setActionCommand(this.DOWNLOAD_MANUSCRIPT);
+
+        
+        myButtonPanel.add(myAddNewManuscriptBtn);
+        myButtonPanel.add(myDeleteManuscriptBtn);
+        myButtonPanel.add(myViewMoreInfoBtn);
+        myButtonPanel.add(myDownloadBtn);
+        
+        myPanel.add(myButtonPanel, BorderLayout.SOUTH);
+        
         myPanel.setOpaque(true);
     }
     
+    /**
+     * Returns the main JPanel for this view
+     * 
+     * PreConditions:
+     * 	JPanel must already have been instantiated
+     * @return a JPanel
+     * @author Ryan Tran
+     * @version 5/27/17
+     */
     public JPanel getMyPanel() {
     	return this.myPanel;
     }
+    
+    /**
+     * Action listener for different buttons.
+     */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String action = e.getActionCommand();
+		
+		switch(action) {
+			case ADD_NEW_MANUSCRIPT:
+				System.out.println(this.myCurrentlySelectedManuscript.getTitle());
+				break;
+			case DELETE_MANSUCRIPT:
+				System.out.println(this.myCurrentlySelectedManuscript.getTitle());
+				break;
+			case VIEW_MORE_INFO:
+				System.out.println(this.myCurrentlySelectedManuscript.getTitle());
+				break;
+			case DOWNLOAD_MANUSCRIPT:
+				System.out.println(this.myCurrentlySelectedManuscript.getTitle());
+				break;
+
+		}
+	}
+	
+	/**
+	 * Sets the view's currently selected manuscript field to the passed in manuscript.
+	 * 
+	 * Preconditions:
+	 * 	Manuscript must be non-null
+	 * 
+	 * @author Ryan Tran
+	 * @version 5/27/17
+	 * @param theManuscript the manuscript to be set to the currently selected manuscript field
+	 */
+	private void setCurrentlySelectedManuscript(Manuscript theManuscript) {
+		this.myCurrentlySelectedManuscript = theManuscript;
+	}
  
     /**
      * Abstract table model class that represents the table's data for the manuscript list table.
@@ -95,8 +227,7 @@ public class ManuscriptListTableView extends Observable {
         private String[] columnNames = {"Title",
                                         "Date Submitted",
                                         "Authors",
-                                        "Delete",
-                                        "Download"};
+                                        "Num. of Reviewers Assigned"};
         /**
          * 2D array of cell data for each row/column
          */
@@ -161,14 +292,13 @@ public class ManuscriptListTableView extends Observable {
          * @return
          */
         private Object[][] generateDataArray(ArrayList<Manuscript> theManuscriptList) {
-        	Object[][] returnList = new Object[theManuscriptList.size()][5];
+        	Object[][] returnList = new Object[theManuscriptList.size()][4];
         	
         	for(int i = 0; i < theManuscriptList.size(); i++) {
         		returnList[i][0] =  theManuscriptList.get(i).getTitle();
         		returnList[i][1] =  theManuscriptList.get(i).getSubmissionDate();
         		returnList[i][2] =  theManuscriptList.get(i).getAuthorEmails().get(0);
-        		returnList[i][3] =  "Delete Btn";
-        		returnList[i][4] =  "Download Btn";
+        		returnList[i][3] =  "# of reviewers assigned";
         	}
         	
         	return returnList;
